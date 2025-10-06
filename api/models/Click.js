27f65708047f8d3b449b3493af47ef4846d3d758
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-10-06 16:40:45 +0530

    Update Click.js

--- api/models/Click.js
+++ api/models/Click.js
@@ -1,1 +1,18 @@
-#
+// File: api/models/Click.js
+```javascript
+const mongoose = require('mongoose');
+
+const ClickSchema = new mongoose.Schema({
+    linkId: { type: mongoose.Schema.Types.ObjectId, ref: 'Link', required: true },
+    timestamp: { type: Date, default: Date.now },
+    ip: { type: String }, // Hashed or partial IP
+    userAgent: { type: String },
+    country: { type: String },
+    referer: { type: String },
+    device: { type: String },
+    browser: { type: String }
+});
+
+ClickSchema.index({ linkId: 1, timestamp: -1 });
+
+module.exports = mongoose.model('Click', ClickSchema);
