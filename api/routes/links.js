a004ca222e91799c162747a03f3cdf6a4cbe33f8
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-10-06 17:05:40 +0530

    Update links.js

--- api/routes/links.js
+++ api/routes/links.js
@@ -1,1 +1,20 @@
-#
+// File: api/routes/links.js
+```javascript
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/auth');
+const {
+    getLinks,
+    getLinkStats,
+    updateLink,
+    deleteLink
+} = require('../controllers/linkController');
+
+// All routes in this file are for authenticated users
+router.use(protect);
+
+router.route('/').get(getLinks);
+router.route('/:id').put(updateLink).delete(deleteLink);
+router.route('/:shortId/stats').get(getLinkStats);
+
+module.exports = router;
