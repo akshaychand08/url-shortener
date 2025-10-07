0d49967ad5678c01020e68f0e74e4adce595c73a
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-10-07 13:55:39 +0530

    Update db.js

--- api/config/db.js
+++ api/config/db.js
@@ -1,24 +1,37 @@
-const mongoose = require('mongoose');
+// File: lib/db.js
 
-// Mongoose connection caching for serverless environments
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  );
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections from growing exponentially
+ * during API Route usage.
+ */
 let cached = global.mongoose;
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function connectDB() {
+async function dbConnect() {
   if (cached.conn) {
     return cached.conn;
   }
 
   if (!cached.promise) {
     const opts = {
-      bufferCommands: false,
+      bufferCommands: false, // This was mentioned in your error log!
     };
 
-    cached.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((mongoose) => {
-      console.log('MongoDB Connected...');
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       return mongoose;
     });
   }
@@ -33,4 +46,5 @@ async function connectDB() {
   return cached.conn;
 }
 
-module.exports = connectDB;
+export default dbConnect;
+
